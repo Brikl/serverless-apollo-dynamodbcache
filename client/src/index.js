35b0d7fa1de8a0cfe8e4419ff7ee118dfc305aaf
Tobias Meixner <meixner.tobias@gmail.com>
2018-06-30 17:54:41 +0700

    Initial commit

---
+++ client/src/index.js
@@ -0,0 +1,58 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import registerServiceWorker from './registerServiceWorker';
+import ApolloClient from "apollo-client";
+import { ApolloConsumer, ApolloProvider } from "react-apollo";
+import { createPersistedQueryLink } from "apollo-link-persisted-queries";
+import { createHttpLink } from "apollo-link-http";
+import { InMemoryCache } from "apollo-cache-inmemory";
+import gql from "graphql-tag";
+import { Query } from 'react-apollo';
+
+const link = createPersistedQueryLink({ 
+    useGETForHashedQueries: true 
+}).concat(createHttpLink({ uri: "http://localhost:4200/graphql" }));
+
+const client = new ApolloClient({
+    cache: new InMemoryCache(),
+    link: link
+});
+
+
+const GET_TODO = gql`
+  query {
+    todo(id: "10") {
+      id
+      content
+    }
+  }
+`
+
+const App = () => (
+    <ApolloProvider client={client}>
+        <ApolloConsumer>
+            {(client) => (
+                <div style={{textAlign: 'center'}}>                                        
+                    <Query query={GET_TODO}>
+                        {({ loading, error, data }) => {
+                        if (loading) return <div>Loading...</div>;
+                        if (error) return <div>Error :(</div>;
+                        return (
+                            <div>
+                            ID: {data.todo.id}
+                            <br/>
+                            Content: {data.todo.content}
+                            </div>
+                        )
+                        }}
+                    </Query>
+                </div>
+            )}    
+        </ApolloConsumer>
+    </ApolloProvider>
+);
+
+ReactDOM.render(<App />, document.getElementById("root"));
+
+registerServiceWorker();
---
+++ handler.js
@@ -0,0 +1,93 @@
+import 'babel-polyfill';
+const { ApolloServer, gql } = require('apollo-server-lambda');
+const { DynamoDBCache } = require('apollo-server-dynamodb');
+
+const AWSXRay = require('aws-xray-sdk');
+const AWS = AWSXRay.captureAWS(require('aws-sdk'));
+
+
+const typeDefs = gql`
+  type Todo @cacheControl(maxAge: 120){
+    id: String!
+    content: String!
+  }
+  type Query {
+    todo(id: String!): Todo
+  }  
+`;
+
+// Provide resolver functions for your schema fields
+const resolvers = {
+  Query: {
+    todo: async (_, { id }, {  }) => {
+      switch (id) {
+        case "1":
+          return {
+            id: id,
+            content: "HAHA1"
+          }
+          break;
+      
+        default:
+          return {
+            id: id,
+            content: "HAHAX"
+          }
+          break;
+      }     
+    }    
+  }
+};
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  tracing: true,
+  cacheControl: {
+    defaultMaxAge: 5,
+    stripFormattedExtensions: false,
+    calculateCacheControlHeaders: false,
+  },
+  persistedQueries: {
+    cache: new DynamoDBCache({
+      region: 'us-east-1',      
+      endpoint: 
+        process.env.IS_OFFLINE==='true' 
+        ?
+        'http://localhost:8000'
+        :
+        ''    
+    },
+    {
+      tableName: 'psq',
+      ttl: 0
+    }
+    ),
+  },
+  // cache: new DynamoDBCache({
+  //   region: 'local',
+  //   endpoint: 
+  //     process.env.IS_OFFLINE==='true' 
+  //     ?
+  //     'http://localhost:8000'
+  //     :
+  //     ''    
+  // }),
+  context: ({ event, context }) => ({
+    headers: event.headers,
+    functionName: context.functionName,
+    event,
+    context,
+  })
+});
+
+exports.graphqlHandler = (event, context, callback) => {
+  const handler = server.createHandler({  
+      cors: {
+        origin: '*',
+        credentials: true,
+      },
+  });
+  // context.callbackWaitsForEmptyEventLoop = false;
+  return handler(event, context, callback);
+}
