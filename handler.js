84595a3f55ddd69c0873addcc98c812b6f779d88
Stephen Jensen <stephen.jensen3@gmail.com>
2019-01-04 17:34:49 -0600

    added dynamo write, removed endpoint

--- handler.js
+++ handler.js
@@ -1,34 +1,34 @@
-import 'babel-polyfill';
+// import 'babel-polyfill';
 const { ApolloServer, gql } = require('apollo-server-lambda');
 const { DynamoDBCache } = require('./dynamoCache');
 const typeDefs = gql`
-  type Todo @cacheControl(maxAge: 120){
+  type Todo @cacheControl(maxAge: 120) {
     id: String!
     content: String!
   }
   type Query {
     todo(id: String!): Todo
-  }  
+  }
 `;
 
 // Provide resolver functions for your schema fields
 const resolvers = {
   Query: {
-    todo: async (_, { id }, {  }) => {
+    todo: async (_, { id }, {}) => {
       switch (id) {
-        case "1":
+        case '1':
           return {
             id: id,
-            content: "HAHA1"
-          }
-      
+            content: 'HAHA1'
+          };
+
         default:
           return {
             id: id,
-            content: "HAHAX"
-          }
-      }     
-    }    
+            content: 'HAHAX'
+          };
+      }
+    }
   }
 };
 
@@ -39,47 +39,47 @@ const server = new ApolloServer({
   cacheControl: {
     defaultMaxAge: 5,
     stripFormattedExtensions: false,
-    calculateCacheControlHeaders: false,
+    calculateCacheControlHeaders: false
   },
   persistedQueries: {
-    cache: new DynamoDBCache({
-      region: 'us-east-1',      
-      endpoint: 
-        process.env.IS_OFFLINE==='true' 
-        ?
-        'http://localhost:8000'
-        :
-        ''    
-    },
-    {
-      tableName: 'psq',
-      ttl: 0
-    }
-    ),
+    cache: new DynamoDBCache(
+      process.env._X_AMZN_TRACE_ID
+        ? {
+            region: 'us-east-1'
+          }
+        : {
+            region: 'us-east-1',
+            endpoint: 'http://localhost:8000'
+          },
+      {
+        tableName: 'psq',
+        ttl: 0
+      }
+    )
   },
   // cache: new DynamoDBCache({
   //   region: 'local',
-  //   endpoint: 
-  //     process.env.IS_OFFLINE==='true' 
+  //   endpoint:
+  //     process.env.IS_OFFLINE==='true'
   //     ?
   //     'http://localhost:8000'
   //     :
-  //     ''    
+  //     ''
   // }),
   context: ({ event, context }) => ({
     headers: event.headers,
     functionName: context.functionName,
     event,
-    context,
+    context
   })
 });
 
 exports.graphqlHandler = (event, context, callback) => {
-  const handler = server.createHandler({  
-      cors: {
-        origin: '*',
-        credentials: true,
-      },
+  const handler = server.createHandler({
+    cors: {
+      origin: '*',
+      credentials: true
+    }
   });
   return handler(event, context, callback);
-}
+};
